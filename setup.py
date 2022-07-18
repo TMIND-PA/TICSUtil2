fd12ad3bada22085c87d33f3e8fcec95d2888149
sunilgoothy <sunil.goothy@tmeic.in>
2022-07-18 17:41:39 +0530

    Functions for configparser.Updated version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TICSUtil2",
-    version="0.6.0",
+    version="0.7.0",
     description="This package is a library of small functions used in TICS development.",
     author="Sunil Goothy",
     author_email="sunil.goothy@tmeic.in",
