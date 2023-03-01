2b9ba3f507f00594b6e8028587ba80776dbdf556
sunilgoothy <sunil.goothy@tmeic.in>
2023-03-01 15:44:40 +0530

    added option to suppress console print msgs

--- TICSUtil2/functions.py
+++ TICSUtil2/functions.py
@@ -7,20 +7,22 @@ def log_time():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
 
 
-def read_yaml_config(filepath):
+def read_yaml_config(filepath, suppress_msgs=False):
     filename = filepath.split(".")[0]
     fileext = filepath.split(".")[1]
     filepath_dev = filename + "_dev" + "." + fileext
     if os.path.isfile(filepath_dev):
         filepath = filepath_dev
-        print(f"Dev config file exists. Using Dev config.")
+        if not suppress_msgs:
+            print(f"Dev config file exists. Using Dev config.")
     config = {}
     try:
         with open(filepath, "r") as cfg:
             config = yaml.safe_load(cfg)
     except Exception as e:
         raise
-    print(f"Configuration read for {filepath} complete.")
+    if not suppress_msgs:
+        print(f"Configuration read for {filepath} complete.")
     return config
 
 
